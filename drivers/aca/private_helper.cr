4f6e0cdf88d408a3c24c2093d49636b987264d3f
Stephen von Takach <steve@acaprojects.com>
2019-05-27 15:19:05 +1000

    add a unique dependency for testing

---
+++ drivers/aca/private_helper.cr
@@ -0,0 +1,9 @@
+module ACA; end
+
+require "base62"
+
+class ACA::PrivateHelper < EngineDriver
+  def used_for_aca_testing
+    "you can delete this file"
+  end
+end
--- drivers/aca/spec_helper.cr
+++
@@ -1,7 +0,0 @@
-module ACA; end
-
-class ACA::SpecHelper < EngineDriver
-  def implemented_in_driver
-    "woot!"
-  end
-end
