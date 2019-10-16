121a3593dbf1b83373d11e8ff8f1150c14e67fe9
Caspian Baska <caspianbaska@gmail.com>
2019-10-16 15:34:27 +1100

    chore: update top level module, refresh readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Private Engine Drivers
+# Private ACAEngine Drivers
 
 Have some code that isn't appropriate to share? Or just like to be in control?
 Manage your own private driver repository.
--- drivers/aca/private_helper.cr
+++ drivers/aca/private_helper.cr
@@ -2,7 +2,7 @@ module ACA; end
 
 require "base62"
 
-class ACA::PrivateHelper < EngineDriver
+class ACA::PrivateHelper < ACAEngine::Driver
   def used_for_aca_testing
     "you can delete this file"
   end
