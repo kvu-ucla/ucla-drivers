fe335884cbb8d7bc843d33fa7b97d7a306b35208
Caspian Baska <caspianbaska@gmail.com>
2020-02-27 14:13:40 +1100

    improvement: add debug output

--- drivers/aca/private_helper.cr
+++ drivers/aca/private_helper.cr
@@ -4,6 +4,7 @@ require "base62"
 
 class ACA::PrivateHelper < ACAEngine::Driver
   def used_for_aca_testing
+    logger.debug("this will be propagated to backoffice!")
     "you can delete this file"
   end
 end
