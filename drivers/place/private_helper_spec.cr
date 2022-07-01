cd4c4448ba7c3eb71148161bbfa8cd3afe7a5be5
Stephen von Takach <steve@place.technology>
2022-07-01 17:01:20 +1000

    feat(place/private_helper_spec): add a spec

--- drivers/place/private_helper_spec.cr
+++ drivers/place/private_helper_spec.cr
@@ -4,4 +4,6 @@ require "placeos-driver/spec"
 # export COMPILE_DRIVER=drivers/place/private_helper_spec.cr
 # crystal build -o exec_name ./src/build.cr
 DriverSpecs.mock_driver "Place::PrivateHelper" do
+  resp = exec :echo, "message"
+  resp.get.should eq "message"
 end
