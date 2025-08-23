856d590e25649ecbb5379716d0f7c221e8c6964f
Kenneth Vu <kvu@it.ucla.edu>
2025-08-23 16:24:46 -0700

    updated

--- drivers/sony/camera/cgi_protocol.cr
+++ drivers/sony/camera/cgi_protocol.cr
@@ -420,7 +420,7 @@ class Sony::Camera::CGI < PlaceOS::Driver
   end
 
   def autoframing?
-    autoframe_status: String = nil
+    autoframe_status: String? = nil
     query("/command/inquiry.cgi?inq=sysinfo", priority: 0) do |response|
       autoframe_status = response["PtzAutoFraming"]?
     end  
