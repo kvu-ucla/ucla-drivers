dd47ad18fe3cfe821a42d9a3434835dc896e4f27
Kenneth Vu <kvu@it.ucla.edu>
2025-09-23 01:35:58 -0700

    updated scheduler since next and current functions are working correctly

--- drivers/zoom/zr_csapi.cr
+++ drivers/zoom/zr_csapi.cr
@@ -36,15 +36,13 @@ class Zoom::ZrCSAPI < PlaceOS::Driver
     @response_delay = setting?(Int32, :milliseconds_until_response) || 500
 
     schedule.clear
+    
     schedule.cron("* * * * *") do
-      schedule.in(rand(1000).milliseconds) do
-        logger.debug { "tick tock" }
-        @current_time = Time.utc.to_unix
-        if list = self[:Bookings]?
-          determine_current_booking(list.as_a)
-          determine_next_booking(list.as_a)
-          determine_active_booking(list.as_a)
-        end
+      sleep rand(1000).milliseconds
+      @current_time = Time.utc.to_unix
+      if list = self[:Bookings]?
+        determine_current_booking(list.as_a)
+        determine_next_booking(list.as_a)
       end
     end
   end
@@ -55,7 +53,6 @@ class Zoom::ZrCSAPI < PlaceOS::Driver
     if list = self[:Bookings]?
       determine_current_booking(list.as_a)
       determine_next_booking(list.as_a) 
-      determine_active_booking(list.as_a)
     end
   end
 
@@ -148,6 +145,11 @@ class Zoom::ZrCSAPI < PlaceOS::Driver
         next
       end
     }.compact
+
+    # find current and next after receiving new booking info
+    updated_bookings = self[:Bookings]
+    determine_current_booking(updated_bookings)
+    determine_next_booking(updated_bookings)
   end
 
   # determine current booking, i.e. booking that is closest to current time
